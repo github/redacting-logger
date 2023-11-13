284e482acb2503900e88d34ef01df69599bd0234
GrantBirki <grantbirki@github.com>
2023-11-13 13:24:37 -0700

    lint

--- lib/redacting_logger.rb
+++ lib/redacting_logger.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require "logger"
 
 # RedactingLogger is a custom logger that extends the standard Logger class.
--- spec/lib/redacting_logger_spec.rb
+++ spec/lib/redacting_logger_spec.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require "logger"
 require "stringio"
 require_relative "../spec_helper"
