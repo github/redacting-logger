c19db583a16c670ac0cc369f649993461f28d7d1
GrantBirki <grantbirki@github.com>
2023-11-13 13:39:05 -0700

    add ghs style tokens

--- lib/patterns/default.rb
+++ lib/patterns/default.rb
@@ -4,6 +4,7 @@
 module Patterns
   DEFAULT = [
     /^ghp_[a-zA-Z0-9]{36}$/, # GitHub Personal Access Token
-    /^github_pat_[a-zA-Z0-9]{22}_[a-zA-Z0-9]{59}$/ # GitHub Personal Access Token (fine-grained)
+    /^github_pat_[a-zA-Z0-9]{22}_[a-zA-Z0-9]{59}$/, # GitHub Personal Access Token (fine-grained)
+    /^ghs_[a-zA-Z0-9]{36}$/ # Temporary GitHub Actions Tokens
   ].freeze
 end
