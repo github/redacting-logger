e73a4441d12d3aedab1888f317fe8553deb5ba47
GrantBirki <grantbirki@github.com>
2023-11-13 14:02:22 -0700

    update regex for PATs

--- lib/patterns/default.rb
+++ lib/patterns/default.rb
@@ -3,7 +3,7 @@
 # This module contains the default patterns to redact.
 module Patterns
   DEFAULT = [
-    /ghp_[a-zA-Z0-9]{36,}/, # GitHub Personal Access Token
+    /ghp_[A-Za-z0-9]{36,}|[0-9A-Fa-f]{40,}/, # GitHub Personal Access Token
     /github_pat_[a-zA-Z0-9]{22}_[a-zA-Z0-9]{59}/, # GitHub Personal Access Token (fine-grained)
     /ghs_[a-zA-Z0-9]{36}/ # Temporary GitHub Actions Tokens
   ].freeze
