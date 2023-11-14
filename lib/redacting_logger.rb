afd297fa5f510340b088819b02b9ce7d5705a702
GrantBirki <grantbirki@github.com>
2023-11-14 14:40:54 -0700

    follow "logger" inputs

--- lib/redacting_logger.rb
+++ lib/redacting_logger.rb
@@ -8,19 +8,25 @@ require_relative "patterns/default"
 class RedactingLogger < Logger
   # Initializes a new instance of the RedactingLogger class.
   #
+  # @param logdev [Object] The log device. Defaults to $stdout.
+  # @param shift_age [Integer] The number of old log files to keep.
+  # @param shift_size [Integer] The maximum logfile size.
   # @param redact_patterns [Array<String>] The patterns to redact from the log messages. Defaults to [].
-  # @param log_device [Object] The log device (file, STDOUT, etc.) to write to. Defaults to STDOUT.
   # @param redacted_msg [String] The message to replace the redacted patterns with.
   # @param use_default_patterns [Boolean] Whether to use the default patterns or not.
   # @param kwargs [Hash] Additional options to pass to the Logger class.
+  #
+  # logdev, shift_age, and shift_size are all using the defaults from the standard Logger class. -> https://github.com/ruby/logger/blob/0996f90650fd95718f0ffe835b965de18654b71c/lib/logger.rb#L578-L580
   def initialize(
+    logdev = $stdout,
+    shift_age = 0,
+    shift_size = 1048576,
     redact_patterns: [],
-    log_device: $stdout,
     redacted_msg: "[REDACTED]",
     use_default_patterns: true,
     **kwargs
   )
-    super(log_device, **kwargs)
+    super(logdev, **kwargs)
     @redact_patterns = redact_patterns
     @redacted_msg = redacted_msg
     @redact_patterns += Patterns::DEFAULT if use_default_patterns
